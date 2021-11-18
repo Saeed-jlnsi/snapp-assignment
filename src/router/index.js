4d2973b26626f192f167f7be9293e861abc25ea2
neo <saeed.jalinousi@gmail.com>
2021-11-18 12:32:19 +0330

    organize project's directory ++ create Passengers & Passenger pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Passengers from '../views/Passengers/Passengers.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Passengers',
+    redirect: '/passengers',
+    component: Passengers
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Passenger',
+    name: 'Passenger',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Passenger/Passenger.vue')
   }
 ]
 
